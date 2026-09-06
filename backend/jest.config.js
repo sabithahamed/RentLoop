6c56524057047487018467657b896f7d3f996e70
Sabith <sabithjiffrey@gmail.com>
2026-09-06 14:34:28 +0530

    Revert "Merge pull request #5 from sabithahamed/copilot/copilotadd-typescript-node-backend"
    This reverts commit 2a074d02126b7f1f8ab9d0545e311119f34d67ed, reversing
    changes made to ab801de8ebdd4fbd5a10d952c5467550f69f28bd.

--- backend/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src/tests'],
-  setupFilesAfterEnv: ['<rootDir>/src/tests/setup.ts'],
-  clearMocks: true
-};
